cf297ab76451f867759d417f44fb91d9a9a247c8
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-11-17 21:47:19 -0400

    Comienzo de la funxion de inicio

---
+++ main.py
@@ -0,0 +1,24 @@
+from models import Historieta
+
+def inicio():
+    # Mensaje de bienvenida con las posibles opciones.
+    print("""
+    Bienvenido a nuestra tienda de historietas ARG.
+
+    Opciones adentro de la aplicación:
+
+    1. Crear una nueva historieta.
+    2. Buscar una historieta por serial.
+    3. Buscar una historieta por palabras del titulo.
+    """)
+
+    # Input para que el usuario ingrese la opción escogida.
+    opcion = input("Ingrese el número de la opción que desea escoger: ")
+
+    try:
+        opcion = int(opcion)
+    except:
+        print(f"La opción ({opcion}) que ingresó no es un número.")
+
+
+    print(opcion)
---
+++ models.py
@@ -0,0 +1,6 @@
+class Historieta:
+    def __init__(self, serial, titulo, precio, stock):
+        self.serial = serial
+        self.titulo = titulo
+        self.precio = precio
+        self.stock = stock
